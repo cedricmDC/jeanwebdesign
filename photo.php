4aa62176c1c0dab6546b6e79be66876bee649d72
cedricm <cedric.morvan.digitalcampus@gmail.com>
2018-03-27 11:59:33 +0200

    affichage de la categorie des tags sur accuei

--- photo.php
+++ photo.php
@@ -4,6 +4,7 @@ require_once 'model/database.php';
 $id = $_GET["id"];
 
 $photo = getPhoto($id);
+$liste_tags = getAllTagsByPhoto($id);
 
 getheader($photo["titre"], "Page des photos Jean Webdesign");
 ?>
@@ -13,6 +14,19 @@ getheader($photo["titre"], "Page des photos Jean Webdesign");
 <?php getmenu(); ?>
     </div>
 </header>
+
+<main class="row col_center">
+
 <h1><?php echo $photo["titre"]?></h1>
+
+<img src="images/<?php echo $photo["image"]; ?>">
+
+<ul>
+    <?php foreach ($liste_tags as $tag) : ?>
+        <li># <?php echo $tag["libelle"] ?></li>
+    <?php endforeach; ?>
+</ul>
+
+</main>
 
 <?php getfooter(); ?>
