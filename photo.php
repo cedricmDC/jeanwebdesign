30f6553e0da552754d7d5c71f1e3b0fca7e13f20
cedricm <cedric.morvan.digitalcampus@gmail.com>
2018-09-24 10:05:35 +0200

    refactoring database

--- photo.php
+++ photo.php
@@ -5,6 +5,7 @@ $id = $_GET["id"];
 
 $photo = getPhoto($id);
 $liste_tags = getAllTagsByPhoto($id);
+$liste_commentaires = getAllCommentairesByPhoto($id);
 
 getheader($photo["titre"], "Page des photos Jean Webdesign");
 ?>
@@ -22,16 +23,36 @@ getheader($photo["titre"], "Page des photos Jean Webdesign");
 
 <img src="images/<?php echo $photo["image"]; ?>">
 
-<ul>
+
     <?php foreach ($liste_tags as $tag) : ?>
-        <li># <?php echo $tag["libelle"] ?></li>
+        <p>#<?php echo $tag["libelle"] ?><p>
     <?php endforeach; ?>
-</ul>
+
 
 <p> <?php echo $photo["description"]; ?></p>
 <p><a href="categorie.php?id=<?php echo $photo['categorie_id']; ?>">
       <?php echo $photo["categorie"]; ?></a></p>
 <p> <?php echo $photo["nb_likes"]; ?> likes</p>
+
+<section class="commentaires">
+    
+    <fieldset>
+        <legend> Poster un commentaire </legend>
+        <form action="insert-commentaire.php" method="POST">
+            <textarea name="contenu" placeholder="Votre message"></textarea>
+            <input type="hidden" name="photo_id" value="<?php echo $id; ?>">
+            <input type="submit">
+        </form>
+    </fieldset>
+    
+    <?php foreach ($liste_commentaires as $commentaire) : ?>
+        <article>
+            <p>Posté le <time><?php echo $commentaire["date_creation"]; ?></time></p>
+            <p><?php echo $commentaire["contenu"] ?></p>
+        </article>
+    <?php endforeach; ?>
+</section>
+
 </main>
 
 <?php getfooter(); ?>
